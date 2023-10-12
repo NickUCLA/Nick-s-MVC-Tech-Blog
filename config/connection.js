63f692501ecbc26d16503f38fbf4043078197793
NickUCLA <133281012+NickUCLA@users.noreply.github.com>
2023-10-11 18:24:17 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -14,9 +14,7 @@ const sequelize = process.env.JAWSDB_URL
       {
         host: "localhost",
         dialect: "mysql",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
+        port: 3306,
       }
     );
 
