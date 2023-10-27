7cad253604d714c961723246a2c3b12dd2543a8e
NickUCLA <133281012+NickUCLA@users.noreply.github.com>
2023-10-26 21:56:04 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 // Allows for environmental variables to be used
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_MARIA_URL
+  ? new Sequelize(process.env.JAWSDB_MARIA_URL)
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
